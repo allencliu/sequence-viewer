edd7cf45e946dc46f9bbd92ebbdc8864b8e191f6
liuac <liuac@miamioh.edu>
2023-11-21 10:12:51 -0500

    Merging Table Sequence

--- SeqViewer.py
+++ SeqViewer.py
@@ -8,7 +8,19 @@ from tkinter.ttk import Treeview
 def is_empty_file(filepath):
     return os.path.getsize(filepath) == 0
 
+def sort_treeview(tree, col, descending):
+    if col == "GC Content":
+        data = [(float(tree.set(item, col).rstrip("%")), item) for item in tree.get_children('')]
+    else:
+        data = [(tree.set(item, col).lower() if col not in ["Length", "A", "T", "G", "C"] else int(tree.set(item, col)), item) for item in tree.get_children('')]
 
+    data.sort(reverse=descending)
+    for index, (val, item) in enumerate(data):
+        tree.move(item, '', index)
+    tree.heading(col, command=lambda: sort_treeview(tree, col, not descending))
+
+
+        
 class ModifiedFASTAViewer:
     def __init__(self, master):
         self.master = master
@@ -18,39 +30,43 @@ class ModifiedFASTAViewer:
         self.upload_button = Button(self.master, text="Upload FASTA", command=self.upload_file)
         self.upload_button.pack(pady=10)
         
-        # Create checkboxes for settings
-        self.spacer = tk.Checkbutton(self.master, text="Spacer", onvalue=1, offvalue=0)
-        self.spacer.pack()
-        
-        self.homopolymer = tk.Checkbutton(self.master, text="Homopolymer", onvalue=1, offvalue=0)
-        self.homopolymer.pack()
-        
-        self.cpg_island = tk.Checkbutton(self.master, text="CpG Island", onvalue=1, offvalue=0)
-        self.cpg_island.pack()
-        
-        self.motif_search = tk.Checkbutton(self.master, text="Motif Search", onvalue=1, offvalue=0)
-        self.motif_search.pack()
-        
-        self.codon_profile = tk.Checkbutton(self.master, text="Codon Profile", onvalue=1, offvalue=0)
-        self.codon_profile.pack()
-        
-        self.printSeqFragment = tk.Checkbutton(self.master, text="Print Sequence Fragment", onvalue=1, offvalue=0)
-        self.printSeqFragment.pack()
-        
-        self.printTargets = tk.Checkbutton(self.master, text="Print Targets", onvalue=1, offvalue=0)
-        self.printTargets.pack()
-        
-        self.alignment = tk.Checkbutton(self.master, text="Aligment", onvalue=1, offvalue=0)
-        self.alignment.pack()
-        
-        self.process_aligned_seq = tk.Checkbutton(self.master, text="Process Aligned Sequence", onvalue=1, offvalue=0)
-        self.process_aligned_seq.pack()
-        
-        self.positional_matrix = tk.Checkbutton(self.master, text="Positional Matrix", onvalue=1, offvalue=0)
-        self.positional_matrix.pack()
-        
-        self.show_positional_matrix = tk.Checkbutton(self.master, text="Show Positional Matrix", onvalue=1, offvalue=0)
-        self.show_positional_matrix.pack()
+        # Create a frame for checkboxes
+        checkbox_frame = tk.Frame(self.master)
+        checkbox_frame.pack()
+
+        # Create checkboxes for settings and pack them horizontally
+        self.spacer = tk.Checkbutton(checkbox_frame, text="Spacer", onvalue=1, offvalue=0)
+        self.spacer.pack(side=tk.LEFT, padx=5)
+
+        self.homopolymer = tk.Checkbutton(checkbox_frame, text="Homopolymer", onvalue=1, offvalue=0)
+        self.homopolymer.pack(side=tk.LEFT, padx=5)
+
+        self.cpg_island = tk.Checkbutton(checkbox_frame, text="CpG Island", onvalue=1, offvalue=0)
+        self.cpg_island.pack(side=tk.LEFT, padx=5)
+
+        self.motif_search = tk.Checkbutton(checkbox_frame, text="Motif Search", onvalue=1, offvalue=0)
+        self.motif_search.pack(side=tk.LEFT, padx=5)
+
+        self.codon_profile = tk.Checkbutton(checkbox_frame, text="Codon Profile", onvalue=1, offvalue=0)
+        self.codon_profile.pack(side=tk.LEFT, padx=5)
+
+        self.printSeqFragment = tk.Checkbutton(checkbox_frame, text="Print Sequence Fragment", onvalue=1, offvalue=0)
+        self.printSeqFragment.pack(side=tk.LEFT, padx=5)
+
+        self.printTargets = tk.Checkbutton(checkbox_frame, text="Print Targets", onvalue=1, offvalue=0)
+        self.printTargets.pack(side=tk.LEFT, padx=5)
+
+        self.alignment = tk.Checkbutton(checkbox_frame, text="Aligment", onvalue=1, offvalue=0)
+        self.alignment.pack(side=tk.LEFT, padx=5)
+
+        self.process_aligned_seq = tk.Checkbutton(checkbox_frame, text="Process Aligned Sequence", onvalue=1, offvalue=0)
+        self.process_aligned_seq.pack(side=tk.LEFT, padx=5)
+
+        self.positional_matrix = tk.Checkbutton(checkbox_frame, text="Positional Matrix", onvalue=1, offvalue=0)
+        self.positional_matrix.pack(side=tk.LEFT, padx=5)
+
+        self.show_positional_matrix = tk.Checkbutton(checkbox_frame, text="Show Positional Matrix", onvalue=1, offvalue=0)
+        self.show_positional_matrix.pack(side=tk.LEFT, padx=5)
         
         # Update based on the checked boxes
         self.update_button = Button(self.master, text="Update")
@@ -65,43 +81,75 @@ class ModifiedFASTAViewer:
         self.listbox_frame.pack(pady=10, padx=20, fill=tk.BOTH, expand=True)
 
         self.listbox_scrollbar = Scrollbar(self.listbox_frame, orient=tk.VERTICAL)
-        self.listbox = Listbox(self.listbox_frame, yscrollcommand=self.listbox_scrollbar.set)
+        self.listbox = Listbox(self.listbox_frame, yscrollcommand=self.listbox_scrollbar.set, height=6)
         self.listbox_scrollbar.config(command=self.listbox.yview)
         self.listbox_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         self.listbox.bind("<<ListboxSelect>>", self.show_sequence)
 
-        # Text box to display the sequence
-        self.sequence_display = Text(self.master, height=10, width=200)
-        self.sequence_display.pack(pady=10, padx=20)
-        
-        # Table view to display sequence info
-        # Create a Treeview for the table view
-        self.tree = Treeview(self.master, columns=("Name", "Description", "Length", "A", "T", "G", "C", "GC Content"))
-        self.tree["show"] = "headings"
-        # self.tree.heading("#0", text="Sequence Headers")
-        self.tree.heading("Name", text="Name")
-        self.tree.column("Name", width=300)
-        self.tree.heading("Description", text="Description")
-        self.tree.column("Description", width=600)
-        self.tree.heading("Length", text="Length")
-        self.tree.column("Length", width=50)
-        self.tree.heading("A", text="A Count")
-        self.tree.column("A", width=50)
-        self.tree.heading("T", text="T Count")
-        self.tree.column("T", width=50)
-        self.tree.heading("G", text="G Count")
-        self.tree.column("G", width=50)
-        self.tree.heading("C", text="C Count")
-        self.tree.column("C", width=50)
-        self.tree.heading("GC Content", text="GC Content")
-        self.tree.column("GC Content", width=50)
-        
-        self.tree.pack(pady=10, padx=20, fill=tk.BOTH, expand=True)
-        self.sequence_table = Text(self.master, height=10, width=200)
-        self.sequence_table.pack(pady=10, padx=20)
 
+        # Create a frame for the sequence_display and its scrollbar
+        self.sequence_display_frame = tk.Frame(self.master)
+        self.sequence_display_frame.pack(pady=10, padx=20)
+
+        # Create a Text widget for sequence_display
+        self.sequence_display = Text(self.sequence_display_frame, height=9, width=200)
+        self.sequence_display.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Create a scrollbar for the sequence_display Text widget
+        self.sequence_display_scrollbar = Scrollbar(self.sequence_display_frame, orient=tk.VERTICAL, command=self.sequence_display.yview)
+        self.sequence_display.config(yscrollcommand=self.sequence_display_scrollbar.set)
+        self.sequence_display_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        
+        
+        # Create a frame for the tree and its scrollbar
+        self.tree_frame = tk.Frame(self.master)
+        self.tree_frame.pack(pady=10, padx=20, fill=tk.BOTH, expand=True)
+
+        # Create a Treeview for the table view
+        self.tree = Treeview(self.tree_frame, columns=("Name", "Description", "Length", "A", "T", "G", "C", "GC Content"), show="headings", height=7)
+        self.tree.heading("Name", text="Name", command=lambda c="Name": sort_treeview(self.tree, c, False))
+        self.tree.column("Name", width=300)
+        self.tree.heading("Description", text="Description", command=lambda c="Description": sort_treeview(self.tree, c, False))
+        self.tree.column("Description", width=600)
+        self.tree.heading("Length", text="Length", command=lambda c="Length": sort_treeview(self.tree, c, False))
+        self.tree.column("Length", width=50)
+        self.tree.heading("A", text="A Count", command=lambda c="A": sort_treeview(self.tree, c, False))
+        self.tree.column("A", width=50)
+        self.tree.heading("T", text="T Count", command=lambda c="T": sort_treeview(self.tree, c, False))
+        self.tree.column("T", width=50)
+        self.tree.heading("G", text="G Count", command=lambda c="G": sort_treeview(self.tree, c, False))
+        self.tree.column("G", width=50)
+        self.tree.heading("C", text="C Count", command=lambda c="C": sort_treeview(self.tree, c, False))
+        self.tree.column("C", width=50)
+        self.tree.heading("GC Content", text="GC Content", command=lambda c="GC Content": sort_treeview(self.tree, c, False))
+        self.tree.column("GC Content", width=50)
+
+        # Create a scrollbar for the tree(Treeview) widget
+        self.tree_scrollbar = Scrollbar(self.tree_frame, orient=tk.VERTICAL, command=self.tree.yview)
+        self.tree.config(yscrollcommand=self.tree_scrollbar.set)
+
+        # Pack the Treeview and its scrollbar into the frame
+        self.tree_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Create a frame for the sequence_table and its scrollbar
+        self.sequence_table_frame = tk.Frame(self.master)
+        self.sequence_table_frame.pack(pady=10, padx=20)
+
+        # Create a Text widget for sequence_table_display
+        self.sequence_table_display = Text(self.sequence_table_frame, height=9, width=200)
+        self.sequence_table_display.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Create a scrollbar for the sequence_table Text widget
+        self.sequence_table_scrollbar = Scrollbar(self.sequence_table_frame, orient=tk.VERTICAL, command=self.sequence_table_display.yview)
+        self.sequence_table_display.config(yscrollcommand=self.sequence_table_scrollbar.set)
+        self.sequence_table_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+
+        self.tree.bind("<<TreeviewSelect>>", self.show_table_sequence)
+        
         # Clear button to reset selections and clear the sequence display
         self.clear_button = Button(self.master, text="Clear", command=self.clear_widget)
         self.clear_button.pack(pady=10)
@@ -184,8 +232,6 @@ class ModifiedFASTAViewer:
             gc_content = (g_count + c_count) / length * 100 if length > 0 else 0
 
             self.tree.insert("", "end", values=(name, description, length, a_count, t_count, g_count, c_count, f"{gc_content:.2f}%"))
-
-
 # Modifying the ModifiedFASTAViewer class to display long sequences in a new window
 
 class UpdatedFASTAViewer(ModifiedFASTAViewer):
@@ -196,7 +242,7 @@ class UpdatedFASTAViewer(ModifiedFASTAViewer):
             return
         header = self.listbox.get(selected[0])
         sequence = self.sequences[header]
-
+        
         # If sequence length exceeds 5000, open it in a new window
         if len(sequence) > 5000:
             self.show_in_new_window(sequence)
@@ -224,48 +270,26 @@ class UpdatedFASTAViewer(ModifiedFASTAViewer):
         self.listbox.selection_clear(0, tk.END)
         self.sequence_display.delete(1.0, tk.END)
         
-    def read_fasta(filename):
-        """Reads a FASTA file and extracts sequence information.
+        # Show sequence based on selected header in table
+    def show_table_sequence(self, sequence):
+        selected_item = self.tree.focus()
+        if not selected_item:
+            return
+        header = ">" + self.tree.item(selected_item, "values")[0] + " " + self.tree.item(selected_item, "values")[1]
+        sequence = self.sequences[header]
+        # print(sequence)
 
-        Args:
-            filename (str): The path/name of the FASTA file to be read.
+        # If sequence length exceeds 5000, open it in a new window
+        if len(sequence) > 5000:
+            self.show_in_new_window(sequence)
+        else:
+            self.sequence_table_display.delete(1.0, tk.END)
+            self.sequence_table_display.insert(tk.END, sequence)
+            # print(self.sequence_table_display.get("1.0", "end-1c"))
 
-        Returns:
-            tuple: A tuple containing three lists: seq_name, seq_desc, and seq_base.
-                - seq_name: List of sequence names.
-                - seq_desc: List of sequence descriptions.
-                - seq_base: List of sequence bases.
-        """
-        # Read the entire contents of the FASTA file into a string.
-        fasta_file = open(filename, "r").read()
-
-        # Split the file into a list of sequences using '>' as the delimiter.
-        name_list = fasta_file.split(">")
-
-        # Initialize empty lists to store sequence names, descriptions, and bases.
-        seq_name = []
-        seq_desc = []
-        seq_base = []
-
-        # Iterate through each sequence in the list (starting from index 1 to skip the empty first element).
-        for seq in name_list[1:]:
-            # Extract the sequence name from the first line.
-            name = seq.split("\n")[0][:].split(" ")[0]
-
-            # Extract the sequence description from the first line, excluding the name.
-            description = " ".join(seq.split("\n")[0][1:].split(" ")[1:])
-
-            # Extract the sequence bases by joining all lines after the first one.
-            sequence = "".join(seq.split("\n")[1:])
-
-            # Append the extracted information to their respective lists.
-            seq_name.append(name)
-            seq_desc.append(description)
-            seq_base.append(sequence)
-
-        # Return a tuple containing the three lists.
-        return (seq_name, seq_desc, seq_base)
-
+    def update_table_display():
+        return
+            
 
 # Display GUI
 root = tk.Tk()
